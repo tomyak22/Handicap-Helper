89d21a9ca45b8f7a28981a2a9c72d1718de411ab
Tom Fenyak <tommyfenyak@gmail.com>
2020-04-14 20:27:35 -0400

    got token from node, created getToken method

--- handicap-helper/src/app/services/auth.service.ts
+++ handicap-helper/src/app/services/auth.service.ts
@@ -6,10 +6,18 @@ import { AuthData } from '../models/auth-data';
   providedIn: 'root'
 })
 export class AuthService {
+  private token: string;
 
   constructor(
     private http: HttpClient
   ) { }
+
+  /**
+   * Gets the token
+   */
+  getToken() {
+    return this.token;
+  }
 
   /**
    * Creates a new user when we use the sign-up component that uses AuthData
@@ -33,9 +41,10 @@ export class AuthService {
    */
   signIn(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post('http://localhost:3000/api/user/sign-in', authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/sign-in', authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
