74a5bc8778fd490aa6a82916b63e9229465f924b
Tom Fenyak <tommyfenyak@gmail.com>
2020-06-14 18:01:02 -0400

    created multiple functions in order to calculate handicap based on number of rounds played

--- handicap-helper/src/app/handicap-index/handicap-index.component.ts
+++ handicap-helper/src/app/handicap-index/handicap-index.component.ts
@@ -26,7 +26,7 @@ export class HandicapIndexComponent implements OnInit {
   ngOnInit(): void {
     this.roundsService.getRounds(null, null);
     this.roundsSub = this.roundsService.getRoundsUpdateListener()
-      .subscribe((roundsData: {rounds: Round[], roundsCount: number}) => {
+      .subscribe((roundsData: { rounds: Round[], roundsCount: number }) => {
         this.totalRounds = roundsData.roundsCount;
         this.handicap = this.getHandicap(roundsData.rounds);
       });
@@ -38,38 +38,93 @@ export class HandicapIndexComponent implements OnInit {
       });
   }
 
+  /**
+   * 
+   * @param rounds 
+   */
   getHandicap(rounds): number {
     let handicap = 0;
-    let totalHandicap = 0;
-    let differential = 0;
-    // if there are less than 10 rounds played we will take the best score
-    // and calculate the handicap with that score only
-    // TODO put in logic to make a copy of the scores into a new array
-    // and sort them from low to high
-    if (this.totalRounds < 10) {
-      for (const round of rounds) {
-        totalHandicap += (round.score - round.rating) * 113 / round.slope;
-      }
-      differential = totalHandicap / this.totalRounds;
-    }
-    // if there are less than 10 rounds but more than 10 rounds played
-    // we will calculate the handicap based on number of rounds played
-    else if (this.totalRounds < 20 && this.totalRounds >= 10) {
-      for (const round of rounds) {
-        totalHandicap += (round.score - round.rating) * 113 / round.slope;
-      }
-      differential = totalHandicap / this.totalRounds;
-    }
-    // If there are more than 20 rounds played we will take the best 10 scores
-    // and calculate the handicap with that.
-    else {
-      for (const round of rounds) {
-        totalHandicap += (round.score - round.rating) * 113 / round.slope;
-      }
-      differential = totalHandicap / this.totalRounds;
-    }
-
-    return differential * 0.96;
+    const previousTwentyRounds = this.getLastNRounds(rounds, 20);
+    const differentials = this.getDifferentials(rounds);
+    const averageDifferential = this.getAverageOfNDifferentials(differentials);
+    handicap = this.roundValue((averageDifferential * 0.96), 1);
+    return handicap;
   }
 
+  /**
+   * Returns all of the differentials for rounds played for a user
+   * @param rounds played by the logged in user
+   */
+  getDifferentials(rounds): number[] {
+    const differentials = [];
+    for (const round of rounds) {
+      const differential = (round.score - round.rating) * 113 / round.slope;
+      differentials.push(differential);
+    }
+    differentials.sort();
+    return differentials;
+  }
+
+  /**
+   * 
+   * @param differentials 
+   */
+  getAverageOfNDifferentials(differentials): number {
+    let lowestDifferentials = 0;
+    if (this.totalRounds <= 5) {
+      lowestDifferentials = 1;
+    }
+    else if (this.totalRounds <= 8) {
+      lowestDifferentials = 2;
+    }
+    else if (this.totalRounds <= 11) {
+      lowestDifferentials = 3;
+    }
+    else if (this.totalRounds <= 14) {
+      lowestDifferentials = 4;
+    }
+    else if (this.totalRounds <= 16) {
+      lowestDifferentials = 5;
+    }
+    else if (this.totalRounds <= 18) {
+      lowestDifferentials = 6;
+    }
+    else if (this.totalRounds === 19) {
+      lowestDifferentials = 7;
+    }
+    else if (this.totalRounds >= 20) {
+      lowestDifferentials = 8;
+    }
+
+    let differentialsSum = 0;
+    for (let i = 0; i < lowestDifferentials; i ++) {
+      differentialsSum += differentials[i];
+    }
+    const averageDifferential = differentialsSum / lowestDifferentials;
+    return averageDifferential;
+  }
+
+  /**
+   * 
+   * @param rounds 
+   * @param nRounds 
+   */
+  getLastNRounds(rounds: Round[], nRounds): Round[] {
+    let latestRounds: Round[];
+    rounds.sort((round1, round2) => {
+      if (new Date(round1.date) < new Date(round2.date)) {
+        return 1;
+      }
+      if (new Date(round1.date) > new Date(round2.date)) {
+        return -1;
+      }
+      return 0;
+    });
+    latestRounds = rounds.slice(0, nRounds);
+    return latestRounds;
+  }
+
+  roundValue(value, decimals) {
+    return Number(Math.round(Number(value + 'e' + decimals)) + 'e-' + decimals);
+  }
 }
