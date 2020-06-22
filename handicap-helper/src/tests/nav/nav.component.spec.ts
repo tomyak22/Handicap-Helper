f21d4428abaca85a077babd86c04e1e712f03524
Tom Fenyak <tommyfenyak@gmail.com>
2020-06-22 18:25:36 -0400

    added test for nav.component

--- handicap-helper/src/tests/nav/nav.component.spec.ts
+++ handicap-helper/src/tests/nav/nav.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from '../../app/nav/nav.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from 'src/app/services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  let mockAuthService: AuthService;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
+      providers: [ AuthService ]
     })
     .compileComponents();
   }));
@@ -17,9 +25,17 @@ describe('NavComponent', () => {
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    mockAuthService = debugElement.injector.get(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call auth service sign out when called', () => {
+    const authServiceSpy = spyOn(mockAuthService, 'signOut').and.callThrough();
+    component.onSignOut();
+    expect(authServiceSpy).toHaveBeenCalled();
+  })
 });
