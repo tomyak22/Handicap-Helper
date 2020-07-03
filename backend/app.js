357f0bb878b15d82e9897ab9dcc7f2554171ceae
Tom Fenyak <tommyfenyak@gmail.com>
2020-07-02 22:00:46 -0400

    clean up for docker

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+// import routers
+const roundsRoutes = require('./routes/rounds');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb://mongo:27017/HandicapHelperDB')
+    .then(() => {
+        console.log('Connected');
+    })
+    .catch(() => {
+        console.log('Error Connecting');
+    });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Request, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS, PUT");
+    next();
+});
+
+app.use('/api/rounds', roundsRoutes);
+app.use('/api/user', userRoutes);
+module.exports = app;
